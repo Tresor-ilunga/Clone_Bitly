d5277929dce1b91f107729c502c60079477225a4
Tresor-ilunga <19im065@esisalama.org>
2022-07-06 21:49:11 +0200

    Modifications des fichier et Ajout du dossier src

---
+++ src/footer.php
@@ -0,0 +1,6 @@
+<!-- PIED DE PAGE -->
+<footer>
+    <img src="assets/logo2.png" alt="logo" id="logo"><br>
+        2022 © Tresor ilunga<br>
+    <a href="#">Contact</a> - <a href="#">À propos</a>
+</footer>
---
+++ src/header.php
@@ -0,0 +1,6 @@
+<!-- EN-TETE -->
+<header>
+    <a href="./">
+        <img src="assets/logo.png" alt="logo" id="logo">
+    </a>
+</header>
